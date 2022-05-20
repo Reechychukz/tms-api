0a91606b56bd45b945d8e312d1f1c9c050180a4e
Richard Chukwuma <richardchukwuma99@gmail.com>
2022-05-20 11:40:30 +0100

    set up db context and entity configurations

---
+++ src/Infrastructure/Persistence/Configurations/AccountConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.DbContext.Configurations
+{
+    public class AccountConfiguration : IEntityTypeConfiguration<Account>
+    {
+        public void Configure(EntityTypeBuilder<Account> builder)
+        {
+            builder.HasOne(x => x.Customer)
+                .WithMany()
+                .HasForeignKey(x => x.Customer.Id);
+
+            builder.HasIndex(x => x.AccountNumber).IsUnique();
+
+            builder.Property(x => x.AccountNumber).IsRequired();
+            builder.Property(x => x.AccountType).IsRequired();
+            builder.Property(x => x.CustomerId).IsRequired();
+        }
+    }
+}
---
+++ src/Infrastructure/Persistence/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.DbContext.Configurations
+{
+    public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.HasIndex(x => x.Email).IsUnique();
+
+            builder.Property(x => x.Email).IsRequired();
+            builder.Property(x => x.FirstName).IsRequired();
+            builder.Property(x => x.LastName).IsRequired();
+            builder.Property(x => x.Address).IsRequired();
+            builder.Property(x => x.PhoneNumber).IsRequired();
+            builder.Property(x => x.State).IsRequired();
+            builder.Property(x => x.Lga).IsRequired();
+        }
+    }
+}
