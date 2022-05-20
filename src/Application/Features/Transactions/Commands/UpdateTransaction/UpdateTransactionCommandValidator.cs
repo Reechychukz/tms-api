a1884acd9f53bd8cb15411793c62e3798b75d621
Richard Chukwuma <richardchukwuma99@gmail.com>
2022-05-20 11:41:07 +0100

    create commands for transaction entity

---
+++ src/Application/Features/Transactions/Commands/UpdateTransaction/UpdateTransactionCommandValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+
+namespace Application.Features.Transactions.Commands.UpdateTransaction
+{
+    public class UpdateTransactionCommandValidator : AbstractValidator<UpdateTransactionCommand>
+    {
+        public UpdateTransactionCommandValidator()
+        {
+            RuleFor(i => i.FromAcct)
+                .NotEmpty().WithMessage("{Sender} is required. ")
+                .NotNull();
+
+            RuleFor(i => i.ToAcct)
+                .NotEmpty().WithMessage("{Receiver} is required. ")
+                .NotNull();
+
+            RuleFor(i => i.TransactionType)
+                .NotEmpty().WithMessage("{Receiver} is required. ");
+
+            RuleFor(i => i.TransactionChannel)
+                .NotEmpty().WithMessage("{Receiver} is required. ");
+        }
+    }
+}
