e1bab7a3263730a215d1ed17aae7a0e58c1b05fd
Richard Chukwuma <richardchukwuma99@gmail.com>
2022-05-20 11:42:52 +0100

    set up enums

---
+++ src/Domain/Enums/EAccountType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace Domain.Enums
+{
+    public enum EAccountType
+    {
+        [Description("SAVINGS")]
+        SAVINGS = 1,
+        [Description("CURRENT")]
+        CURRENT = 2
+    }
+}
