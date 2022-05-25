62fe2383b610b803430ec43dff464638d8aaec0b
Richard Chukwuma <richardchukwuma99@gmail.com>
2022-05-25 08:56:24 +0100

    unit test to get customer list

---
+++ src/TmsTest/AccountType/Queries/GetCustomersListRequestHandler.cs
@@ -0,0 +1,38 @@
+﻿using Application.Contracts.Persistence;
+using Application.Features.Customers.Queries.GetCustomersList;
+using Application.Mappings;
+using AutoMapper;
+using Moq;
+using Shouldly;
+using TmsTest.Mocks;
+
+namespace TmsTest.AccountType.Queries
+{
+    public class GetCustomersListRequestHandler
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<ICustomerRepository> _mockCustomerRepo;
+
+        public GetCustomersListRequestHandler()
+        {
+            _mockCustomerRepo = MockCustomerRepository.GetListCustomersRepository();
+
+            var mapperConfig = new MapperConfiguration(c =>
+            {
+                c.AddProfile<MappingProfile>();
+            });
+            _mapper = mapperConfig.CreateMapper();
+        }
+
+        [Fact]
+        public async Task GetCutomersListTest()
+        {
+            var handler = new GetCustomersListQueryHandler(_mockCustomerRepo.Object, _mapper);
+            string firstName = "John";
+            var result = await handler.Handle(new GetCustomersListQuery(firstName), CancellationToken.None);
+
+            result.ShouldBeOfType<List<CustomersDto>>();
+            result.Count.ShouldBe(2);
+        }
+    }
+}
---
+++ src/TmsTest/Mocks/MockCustomerRepository.cs
@@ -0,0 +1,52 @@
+﻿using Application.Contracts.Persistence;
+using Domain.Entities;
+using Moq;
+
+namespace TmsTest.Mocks
+{
+    public static class MockCustomerRepository
+    {
+        public static Mock<ICustomerRepository> GetListCustomersRepository()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer
+                {
+                    Id = Guid.NewGuid(),
+                    Address = "Lagos",
+                    Email = "johndoe@gmail.com",
+                    FirstName = "John",
+                    LastName = "Doe",
+                    State = "Lagos",
+                    Gender = "Male",
+                    PhoneNumber = "09029232x",
+                    CreatedAt = DateTime.Now
+                },
+                new Customer
+                {
+                    Id = Guid.NewGuid(),
+                    Address = "Lagos",
+                    Email = "johnde@gmail.com",
+                    FirstName = "John",
+                    LastName = "Doe",
+                    State = "Lagos",
+                    Gender = "Male",
+                    PhoneNumber = "09029232x",
+                    CreatedAt = DateTime.Now
+                }
+
+            };
+            var mockRepo = new Mock<ICustomerRepository>();
+            string firstName = "John";
+            mockRepo.Setup(r => r.GetCustomersByFIrstName(firstName)).ReturnsAsync(customers);
+
+            mockRepo.Setup(r => r.AddAsync(It.IsAny<Customer>())).ReturnsAsync((Customer customer) =>
+            {
+                customers.Add(customer);
+                return customer;
+            });
+
+            return mockRepo;
+        }
+    }
+}
